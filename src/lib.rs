cea35f46215b4f12e0c958ed9da9aff096233351
Koen van Wel <koenvwel@gmail.com>
2021-02-23 19:12:45 +0100

    Add a borrow in comments

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@
 //!   let expression = "0   30   9,12,15     1,15       May-Aug  Mon,Wed,Fri  2018/2";
 //!   let schedule = Schedule::from_str(expression).unwrap();
 //!   println!("Upcoming fire times:");
-//!   for datetime in schedule.upcoming(Utc).take(10) {
+//!   for datetime in schedule.upcoming(&Utc).take(10) {
 //!     println!("-> {}", datetime);
 //!   }
 //! }
